4877236d03e6f8597555f2d56739e38902517235
Andreas Klebinger <klebinger.andreas@gmx.at>
2018-02-20 12:01:04 +0100

    Set priority of proccess to high, allow crition arguments with --CRIT

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,76 @@
 module Main where
 
+import Data.List
+
 import Criterion.Main
-import System.Process
+import System.Process hiding (ProcessHandle)
 import System.Environment
 import Control.Monad
 import Control.DeepSeq
+import Criterion.Types
+
+import System.Win32.Types
+import System.Win32.Thread
+import System.Win32.Process
+import Foreign.C.Types
+import Foreign.Ptr
+
+
+
+
+foreign import ccall  "windows.h SetThreadAffinityMask"
+    c_SetThreadAffinityMask :: THANDLE -> DDWORD -> IO DWORD_PTR
+
+foreign import ccall  "windows.h SetPriorityClass"
+    c_SetPriorityClass :: ProcessHandle -> DWORD -> IO CBool
+
+{-
+
+BOOL WINAPI SetPriorityClass(
+  _In_ HANDLE hProcess,
+  _In_ DWORD  dwPriorityClass
+);
+
+
+DWORD_PTR WINAPI SetThreadAffinityMask(
+  _In_ HANDLE    hThread,
+  _In_ DWORD_PTR dwThreadAffinityMask
+);
+
+BOOL WINAPI SetThreadPriority(
+  _In_ HANDLE hThread,
+  _In_ int    nPriority
+); -}
+
+thread_mask :: DDWORD
+thread_mask = 4
+
+setupEnv :: IO ()
+setupEnv = do
+  th <- getCurrentThread
+  ph <- getCurrentProcess
+  c_SetPriorityClass ph 0x00000080 -- HIGH_PRIORITY_CLASS
+  --print =<< c_SetThreadAffinityMask th thread_mask
+
+  return ()
 
 
 executeFile :: FilePath -> [String] -> IO String
 executeFile path args =
   readProcess path args []
 
-splitArgs :: [String] -> [[String]]
+benchConfig :: Config
+benchConfig =
+  defaultConfig
+    { timeLimit = 3
+    }
+
+splitArgs :: [String] -> ([[String]])
 splitArgs [] = []
-splitArgs args = 
+splitArgs args =
   let (a1, remainingArgs) = span (/= "--") args
   in
-  a1:(if null remainingArgs then  [] else splitArgs (tail remainingArgs)) 
+  a1:(if null remainingArgs then  [] else splitArgs (tail remainingArgs))
 
 createBenchmark :: [String] -> Benchmark
 createBenchmark x@(file:parameters) =
@@ -24,10 +78,13 @@ createBenchmark x@(file:parameters) =
 
 main :: IO ()
 main = do
+  setupEnv
   args <- getArgs
-  let execArgs = splitArgs args
+  let (bargs, critArgs') = span ( /= "--CRIT") args
+  let critArgs = if null critArgs' then [] else tail critArgs'
+  let execArgs = splitArgs bargs
   when (null execArgs) $ fail "No executable given"
   deepseq execArgs $ return ()
   let benchmarks = map createBenchmark execArgs
-  withArgs ["-v 1"] $ defaultMain benchmarks
+  withArgs (critArgs) $ defaultMainWith benchConfig benchmarks
 
